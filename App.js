8de6ac8ed8be269f1cb3a85af135d4114214c25f
TheRealAt0th3T <47465309+TheRealAt0th3T@users.noreply.github.com>
2023-11-30 13:25:40 -0700

    skeleton created with sample data

--- App.js
+++ App.js
@@ -1,23 +1,40 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native"; //SafeAreaView -- sets view where user can actually see (skips over the notch forexample on top of iphone)
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
+//SafeAreaView -- sets view where user can actually see (skips over the notch forexample on top of iphone)
+//ScrollView -- added for if num of attempts goes beyond screen length
 import { colors } from "./src/constants";
 import Keyboard from "./src/components/Keyboard";
 
+const NUM_OF_ATTEMPTS = 6;
+
 export default function App() {
+  const word = "hello";
+  const characters = word.split("");
+
+  const rows = new Array(NUM_OF_ATTEMPTS).fill(
+    new Array(characters.length).fill("a".toUpperCase())
+  ); //creating 6 rows with initial input empty
+
+  onKeyPressed = (key) => {
+    console.log(key);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="light" />
       <Text style={styles.title}>WORDLE</Text>
-      <View style={styles.map}>
-        <View style={styles.row}>
-          <View style={styles.cell} />
-          <View style={styles.cell} />
-          <View style={styles.cell} />
-          <View style={styles.cell} />
-          <View style={styles.cell} />
-        </View>
-      </View>
-      <Keyboard />
+      <ScrollView style={styles.map}>
+        {rows.map((row, i) => (
+          <View key={i} style={styles.row}>
+            {row.map((cell, index) => (
+              <View key={index} style={styles.cell}>
+                <Text style={styles.cellText}>{cell}</Text>
+              </View>
+            ))}
+          </View>
+        ))}
+      </ScrollView>
+      <Keyboard onKeyPressed={onKeyPressed} />
     </SafeAreaView>
   );
 }
@@ -35,21 +52,28 @@ const styles = StyleSheet.create({
     letterSpacing: 5,
   },
   map: {
-    borderColor: "red",
-    borderWidth: 3,
     alignSelf: "stretch",
     height: 100,
+    marginTop: 20,
   },
   row: {
     alignSelf: "stretch",
-    height: 50,
     flexDirection: "row",
+    justifyContent: "center",
   },
   cell: {
-    borderColor: colors.grey,
+    borderColor: colors.darkgrey,
     borderWidth: 2,
     flex: 1, //splits horizontal space evenly between cells
     aspectRatio: 1, //creates square cells
     margin: 5,
+    maxWidth: 65,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cellText: {
+    color: colors.lightgrey,
+    fontSize: 32,
+    fontWeight: "bold",
   },
 });
